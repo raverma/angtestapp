9af5a97ad155bbb2d9cb765c5156d1269d82cee7
Rahul Verma <rahulv74@hotmail.com>
2020-06-28 18:26:20 +0530

    Using observables to fetch posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,27 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
-
+import { Subscription}  from 'rxjs';
 @Component({
     selector: 'app-post-list',
     templateUrl: './post-list.component.html',
     styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit{
+export class PostListComponent implements OnInit, OnDestroy{
 /*posts = [
     {title: "First post", content: "This is the content for First post"},
     {title: "Second post", content: "This is the content for Second post"},
     {title: "Third post", content: "This is the content for Third post"},
 ]*/
 posts: Post[] = [];
+postsSub: Subscription;
 //postsServ : PostsService;   //not required if public is used before the constructor parameter
 constructor(public postsService: PostsService){
    
 }
 
 ngOnInit(){
-    this.posts = this.postsService.getPosts();
+    //this.posts = this.postsService.getPosts();
+
+    this.postsSub = this.postsService.getPostUpdatedListener().subscribe((posts: Post[])=> {
+        this.posts = posts;
+    });
+}
+
+ngOnDestroy() {
+    this.postsSub.unsubscribe();
 }
 
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,15 +1,21 @@
 import { Post} from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
     private posts: Post[] = [];
+    private postsUpdated = new Subject<Post[]>();
+    //getPosts() {
+    //    return [...this.posts];
+    //}
 
-    getPosts() {
-        return this.posts;
+    getPostUpdatedListener() {
+        return this.postsUpdated.asObservable();
     }
 
     addPost(post: Post) {
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
     }
 }
