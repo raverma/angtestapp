9e7f0fb1e391d2d43d9e2813cab8a6e2788e5008
Rahul Verma <rahulv74@hotmail.com>
2020-08-02 21:52:23 +0530

    Added route guards to prevent direct url access when not authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth/auth.guard';
 import { PostCreateComponent } from './posts/post-create/posts-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent} from './auth/login/login.component';
@@ -7,14 +8,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     { path: '', component: PostListComponent},
-    { path: 'create', component: PostCreateComponent}, 
-    { path: 'edit/:postId', component: PostCreateComponent},
+    { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard]}, 
+    { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard]},
     { path: 'login', component: LoginComponent},
     { path: 'signup', component: SignupComponent}
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
 export class AppRoutingModule {
 
